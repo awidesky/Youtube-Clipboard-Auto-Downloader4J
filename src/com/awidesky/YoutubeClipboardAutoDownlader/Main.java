0a1276f4f902d1be32c280e6fcf84d60a511a28e
awidesky <awidesky@naver.com>
2020-07-16 21:38:46 +0900

    initial commit & version 1.0

---
+++ src/com/awidesky/YoutubeClipboardAutoDownlader/Main.java
@@ -0,0 +1,69 @@
+package com.awidesky.YoutubeClipboardAutoDownlader;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.FlavorEvent;
+import java.awt.datatransfer.FlavorListener;
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+/** Main class */
+public class Main {
+
+	private static ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	
+	public static void main(String[] args) throws Exception {
+
+		JFileChooser jfc = new JFileChooser();
+        jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jfc.setDialogTitle("Choose directory for saving music!");
+        jfc.showDialog(new JFrame(), null);
+        File dir = jfc.getSelectedFile();
+		
+		Toolkit.getDefaultToolkit().getSystemClipboard().addFlavorListener(new FlavorListener() { 
+		
+				@Override 
+			    public void flavorsChanged(FlavorEvent e) {
+
+					executorService.submit(() -> {
+						
+						try {
+						
+							Thread.sleep(50);
+						
+							String data = (String)Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+					
+							if (data.startsWith("https://www.youtu")) {
+			    	  
+								log("Receved a link from your clipboard : " + data);
+								YoutubeAudioDownloader.download(data, dir.getAbsolutePath());
+
+							}
+						
+						} catch(Exception err) {
+						
+							log(err.toString());
+						
+						}
+			   
+					});
+				}
+		
+		});
+		
+		log("Listning clipboard...");
+		
+	}
+	
+	
+	public static void log(String data) {
+		
+		System.out.println(data);
+		
+	}
+
+}
---
+++ src/com/awidesky/YoutubeClipboardAutoDownlader/YoutubeAudioDownloader.java
@@ -0,0 +1,75 @@
+package com.awidesky.YoutubeClipboardAutoDownlader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class YoutubeAudioDownloader {
+
+	private static final String youtubedlpath = new File("").getAbsolutePath() + "\\resources\\ffmpeg\\bin"; //Thread.currentThread().getContextClassLoader().getResource("ffmpeg/bin").getPath();
+	private static File downloadPath;
+	
+	static {
+		
+		if (!new File(youtubedlpath + "\\youtube-dl.exe").exists()) { throw new Error("youtube-dl.exe does not exist!"); }
+
+	}
+	
+	static void download(String url, String path) throws Exception {
+		
+		downloadPath = new File(path);
+		
+		try {
+			
+			//Main.log(downloadPath.getAbsolutePath());
+			ProcessBuilder pb = new ProcessBuilder(youtubedlpath + "\\youtube-dl", "-x", "--audio-format", "mp3", "--audio-quality", "0",  url);
+			pb.directory(new File(youtubedlpath));
+			Process p = pb.start();
+	    
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = null;
+			
+			while((line = br.readLine ()) != null) {
+				
+				Main.log(line);
+				
+			}
+			
+			p.waitFor();
+			
+			//Thread.currentThread().sleep(100);
+			
+			Main.log("founding downloaded file...");
+			
+			File[] fileList = new File(youtubedlpath).listFiles(new FilenameFilter() {
+				
+				@Override
+				public boolean accept(File dir, String name) {
+					// TODO Auto-generated method stub
+					return name.endsWith("mp3");
+				}
+				
+			});
+			
+			if(fileList.length ==0 ) { throw new RuntimeException("Didn't dowload any files!"); }
+			
+			for(File f : fileList) {
+				
+				Files.copy(f.toPath(), Paths.get(downloadPath.getAbsolutePath() + "\\" + f.getName()) ,StandardCopyOption.REPLACE_EXISTING);
+				Files.delete(f.toPath());
+				
+			}
+			
+			Main.log("Done!");
+			
+		} catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+		
+	}
+	
+}
