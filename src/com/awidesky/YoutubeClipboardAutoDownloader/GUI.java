f2bb53bd3e046f8f28ead66a9073e7d83a0e3460
awidesky <awidesky@naver.com>
2020-07-27 21:52:38 +0900

    building javaFX project is horrible idea, so use swing.

---
+++ src/com/awidesky/YoutubeClipboardAutoDownloader/GUI.java
@@ -0,0 +1,15 @@
+package com.awidesky.YoutubeClipboardAutoDownloader;
+
+import javax.swing.JFrame;
+// https://m.blog.naver.com/PostView.nhn?blogId=javaking75&logNo=140157948347&proxyReferer=https:%2F%2Fwww.google.com%2F 
+// https://m.blog.naver.com/PostView.nhn?blogId=sks6624&logNo=150165603219&proxyReferer=https:%2F%2Fwww.google.com%2F
+public class GUI extends JFrame {
+
+	/**
+	 * serial version UID
+	 */
+	private static final long serialVersionUID = 7447706573178166218L;
+
+	
+
+}
