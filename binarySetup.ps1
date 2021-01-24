065e866b95ad2d0b8645a981a3b8d15049bb1e3f
awidesky <awidesky@naver.com>
2021-01-24 17:22:43 +0900

    add binarySetup.ps1

---
+++ binarySetup.ps1
@@ -0,0 +1,10 @@
+﻿
+Set-Location $PSScriptRoot
+
+New-Item -Type Directory -Path YoutubeAudioAutoDownloader-resources
+
+(new-object System.Net.WebClient).DownloadFile('https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip', '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip')
+[System.IO.Compression.ZipFile]::ExtractToDirectory(".\YoutubeAudioAutoDownloader-resources\ffmpeg.zip", ".\YoutubeAudioAutoDownloader-resources\ffmpeg")
+Remove-Item ".\YoutubeAudioAutoDownloader-resources\ffmpeg.zip"
+
+(new-object System.Net.WebClient).DownloadFile('https://github.com/ytdl-org/youtube-dl/releases/download/2021.01.16/youtube-dl.exe', '.\YoutubeAudioAutoDownloader-resources\ffmpeg\bin\youtube-dl.exe')
