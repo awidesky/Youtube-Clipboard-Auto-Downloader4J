ab919ea4993f97dd9d1f77e352e030c4645f4f55
awidesky <awidesky@naver.com>
2021-01-31 17:51:27 +0900

    modify binarySetup.ps1

--- binarySetup.ps1
+++ binarySetup.ps1
@@ -1,10 +1,22 @@
-﻿
+﻿#Set-ExecutionPolicy RemoteSigned
+
 Set-Location $PSScriptRoot
 
-New-Item -Type Directory -Path YoutubeAudioAutoDownloader-resources
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+Write-output "Making Directory..."
+New-Item -Force -Type Directory -Path YoutubeAudioAutoDownloader-resources
+
+Write-output "Downloading ffmpeg..."
 (new-object System.Net.WebClient).DownloadFile('https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip', '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip')
-[System.IO.Compression.ZipFile]::ExtractToDirectory(".\YoutubeAudioAutoDownloader-resources\ffmpeg.zip", ".\YoutubeAudioAutoDownloader-resources\ffmpeg")
-Remove-Item ".\YoutubeAudioAutoDownloader-resources\ffmpeg.zip"
 
+Write-output "Expanding Archive..."
+Expand-Archive -LiteralPath '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip' -DestinationPath '.\YoutubeAudioAutoDownloader-resources'
+Rename-Item '.\YoutubeAudioAutoDownloader-resources\ffmpeg-4.3.1-2021-01-26-essentials_build' -NewName 'ffmpeg'
+Remove-Item '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip'
+
+Write-output "ownloading youtube-dl..."
 (new-object System.Net.WebClient).DownloadFile('https://github.com/ytdl-org/youtube-dl/releases/download/2021.01.16/youtube-dl.exe', '.\YoutubeAudioAutoDownloader-resources\ffmpeg\bin\youtube-dl.exe')
+
+
+Write-output "Done!"
