c5faf374a57cd19f73ae1eb27284db47d2d24598
Eugene Hong <awidesky@naver.com>
2021-02-17 19:14:40 +0900

    edit binarySetup.ps1 and add binarySetup.bat

--- binarySetup.ps1
+++ binarySetup.ps1
@@ -1,27 +1,37 @@
 ﻿#Set-ExecutionPolicy RemoteSigned
 
 Set-Location $PSScriptRoot
+$ErrorActionPreference = "Inquire"
 
 $ffmpeg_version='ffmpeg-4.3.1-2021-01-26-essentials_build'
 $youtubedl_version='2021.01.16'
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Write-output "Making Directory..."
+
+
+if (Test-Path -Path '.\YoutubeAudioAutoDownloader-resources') { Remove-Item -Path '.\YoutubeAudioAutoDownloader-resources' -Recurse }
+
+
+Write-output "  Making Directory..."
 New-Item -Force -Type Directory -Path YoutubeAudioAutoDownloader-resources
 
-Write-output "Downloading ffmpeg..."
+
+if (Test-Path -Path '.\YoutubeAudioAutoDownloader-resources\ydlver.txt') { Remove-Item -Path '.\YoutubeAudioAutoDownloader-resources\ydlver.txt' }
+
+Set-content -Path '.\YoutubeAudioAutoDownloader-resources\ydlver.txt' -Value $youtubedl_version.Replace(".", "")
+
+Write-output "  Downloading ffmpeg..."
 (new-object System.Net.WebClient).DownloadFile("https://www.gyan.dev/ffmpeg/builds/packages/$ffmpeg_version.zip", '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip')
 
-Write-output "Expanding Archive..."
+Write-output "  Expanding Archive..."
 Expand-Archive -LiteralPath '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip' -DestinationPath '.\YoutubeAudioAutoDownloader-resources'
+Write-Output "  Renaming Archive..."
 Rename-Item ".\YoutubeAudioAutoDownloader-resources\$ffmpeg_version" -NewName "ffmpeg"
 Remove-Item '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip'
 
-Write-output "Downloading youtube-dl..."
+Write-output "  Downloading youtube-dl..."
 (new-object System.Net.WebClient).DownloadFile("https://github.com/ytdl-org/youtube-dl/releases/download/$youtubedl_version/youtube-dl.exe", '.\YoutubeAudioAutoDownloader-resources\ffmpeg\bin\youtube-dl.exe')
 
 
-Write-output "Done!"
-
-pause
+Write-output "  Done!"
