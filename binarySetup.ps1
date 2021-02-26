700f143ddabb33581f8b83f22f176193fd425ddb
Eugene Hong <awidesky@naver.com>
2021-02-26 17:24:56 +0900

    finish binarySetup.ps1

--- binarySetup.ps1
+++ binarySetup.ps1
@@ -2,36 +2,43 @@
 
 Set-Location $PSScriptRoot
 $ErrorActionPreference = "Inquire"
+$youtubedl_version="2021.01.16"
 
-$ffmpeg_version='ffmpeg-4.3.1-2021-01-26-essentials_build'
-$youtubedl_version='20210116'
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-
-
-if (Test-Path -Path '.\YoutubeAudioAutoDownloader-resources') { Remove-Item -Path '.\YoutubeAudioAutoDownloader-resources' -Recurse }
-
-
-Write-output "  Making Directory..."
-New-Item -Force -Type Directory -Path YoutubeAudioAutoDownloader-resources
+if (!(Test-Path -Path '.\YoutubeAudioAutoDownloader-resources')) {
+    Write-output "  Making Directory..."
+    New-Item -Force -Type Directory -Path YoutubeAudioAutoDownloader-resources
+} elseif (Test-Path -Path '.\YoutubeAudioAutoDownloader-resources\ffmpeg') {
+    Remove-Item -Path '.\YoutubeAudioAutoDownloader-resources\ffmpeg' -Recurse
+}
 
 
-if (Test-Path -Path '.\YoutubeAudioAutoDownloader-resources\ydldate.txt') { Remove-Item -Path '.\YoutubeAudioAutoDownloader-resources\ydldate.txt' }
+if (Test-Path -Path '.\YoutubeAudioAutoDownloader-resources\ydlver.txt') {
+    $youtubedl_version = Get-Content ".\YoutubeAudioAutoDownloader-resources\ydlver.txt" -Encoding UTF8 -Raw
+    Remove-Item -Path '.\YoutubeAudioAutoDownloader-resources\ydlver.txt'
+}
 
-Set-content -Path '.\YoutubeAudioAutoDownloader-resources\ydldate.txt' -Value $youtubedl_version
+Write-output "  youtube-dl version : $youtubedl_version"
+
+Set-content  -NoNewline -Path '.\YoutubeAudioAutoDownloader-resources\ydlver.txt' -Value "$youtubedl_version" -Encoding UTF8
 
 Write-output "  Downloading ffmpeg..."
-(new-object System.Net.WebClient).DownloadFile("https://www.gyan.dev/ffmpeg/builds/packages/$ffmpeg_version.zip", '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip')
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip' -OutFile '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip'
 
 Write-output "  Expanding Archive..."
 Expand-Archive -LiteralPath '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip' -DestinationPath '.\YoutubeAudioAutoDownloader-resources'
+
 Write-Output "  Renaming Archive..."
-Rename-Item ".\YoutubeAudioAutoDownloader-resources\$ffmpeg_version" -NewName "ffmpeg"
-Remove-Item '.\YoutubeAudioAutoDownloader-resources\ffmpeg.zip'
+$dirs = Get-ChildItem '.\YoutubeAudioAutoDownloader-resources' -filter "ffmpeg*" -Directory
+
+foreach($d in $dirs) {
+    Rename-Item "$PSScriptRoot\YoutubeAudioAutoDownloader-resources\$d" -NewName "ffmpeg" -Force
+}
+Remove-Item -Path ".\YoutubeAudioAutoDownloader-resources\ffmpeg.zip"
 
 Write-output "  Downloading youtube-dl..."
-(new-object System.Net.WebClient).DownloadFile("https://github.com/ytdl-org/youtube-dl/releases/download/$youtubedl_version/youtube-dl.exe", '.\YoutubeAudioAutoDownloader-resources\ffmpeg\bin\youtube-dl.exe')
+Invoke-WebRequest -Uri "https://github.com/ytdl-org/youtube-dl/releases/download/$youtubedl_version/youtube-dl.exe" -OutFile  '.\YoutubeAudioAutoDownloader-resources\ffmpeg\bin\youtube-dl.exe'
 
 
 Write-output "  Done!"
