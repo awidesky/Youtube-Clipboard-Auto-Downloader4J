c0926f9933baab69b304ac51d8beeb0973355f75
Eugene Hong <awidesky@naver.com>
2021-03-29 12:45:17 +0900

    make field name of GUI class more readable

--- src/com/awidesky/YoutubeClipboardAutoDownloader/gui/GUI.java
+++ src/com/awidesky/YoutubeClipboardAutoDownloader/gui/GUI.java
@@ -28,9 +28,9 @@ public class GUI {
 	
 
 	private JFrame mainFrame;
-	private JButton btn_browse, btn_cleanCompleted, btn_cleanAll;
-	private JLabel tlb_format, tlb_quality, tlb_path;
-	private JTextField jft_path;
+	private JButton browse, cleanCompleted, cleanAll;
+	private JLabel format, quality, path;
+	private JTextField pathField;
 	private JComboBox<String> cb_format, cb_quality;
 	private JFileChooser jfc = new JFileChooser();
 	private JTable table;
@@ -101,19 +101,19 @@ public class GUI {
 		jfc.setDialogTitle("Choose directory to save music!");
 		jfc.setCurrentDirectory(new File(Main.getProperties().getSaveto()));
 
-		tlb_format = new JLabel("Format :");
-		tlb_quality = new JLabel("Audio Quality :");
-		tlb_path = new JLabel("Save to :");
-		jft_path = new JTextField(Main.getProperties().getSaveto());
+		format = new JLabel("Format :");
+		quality = new JLabel("Audio Quality :");
+		path = new JLabel("Save to :");
+		pathField = new JTextField(Main.getProperties().getSaveto());
 
-		jft_path.addActionListener((e) -> {
+		pathField.addActionListener((e) -> {
 
-			Main.getProperties().setSaveto(jft_path.getText());
+			Main.getProperties().setSaveto(pathField.getText());
 
 		});
 
-		btn_browse = new JButton("Browse...");
-		btn_browse.addActionListener((e) -> {
+		browse = new JButton("Browse...");
+		browse.addActionListener((e) -> {
 
 			if (jfc.showDialog(new JFrame(), null) != JFileChooser.APPROVE_OPTION) {
 				JOptionPane.showMessageDialog(null, "Please choose a directory!", "ERROR!",
@@ -123,14 +123,14 @@ public class GUI {
 
 			String path = jfc.getSelectedFile().getAbsolutePath();
 			Main.getProperties().setSaveto(path);
-			jft_path.setText(path);
+			pathField.setText(path);
 			jfc.setCurrentDirectory(new File(path));
 
 		});
-		btn_cleanCompleted = new JButton("clean completed");
-		btn_cleanCompleted.addActionListener((e) -> { TaskStatusModel.getinstance().clearDone(); });
-		btn_cleanAll = new JButton("clean all");
-		btn_cleanAll.addActionListener((e) -> { TaskStatusModel.getinstance().clearAll(); });
+		cleanCompleted = new JButton("clean completed");
+		cleanCompleted.addActionListener((e) -> { TaskStatusModel.getinstance().clearDone(); });
+		cleanAll = new JButton("clean all");
+		cleanAll.addActionListener((e) -> { TaskStatusModel.getinstance().clearAll(); });
 
 		cb_format = new JComboBox<>(new String[] { "mp3", "best", "aac", "flac", "m4a", "opus", "vorbis", "wav" });
 		cb_quality = new JComboBox<>(new String[] { "0(best)", "1", "2", "3", "4", "5", "6", "7", "8", "9(worst)" });
@@ -162,26 +162,26 @@ public class GUI {
 		scrollPane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setBounds(8, 122, 600, 280);
 
-		btn_browse.setBounds(523, 76, btn_browse.getPreferredSize().width, btn_browse.getPreferredSize().height);
-		btn_cleanCompleted.setBounds(14, 418, btn_cleanCompleted.getPreferredSize().width, btn_cleanCompleted.getPreferredSize().height);
-		btn_cleanAll.setBounds(142, 418, btn_cleanAll.getPreferredSize().width, btn_cleanAll.getPreferredSize().height);
+		browse.setBounds(523, 76, browse.getPreferredSize().width, browse.getPreferredSize().height);
+		cleanCompleted.setBounds(14, 418, cleanCompleted.getPreferredSize().width, cleanCompleted.getPreferredSize().height);
+		cleanAll.setBounds(142, 418, cleanAll.getPreferredSize().width, cleanAll.getPreferredSize().height);
 		
-		tlb_format.setBounds(26, 23, tlb_format.getPreferredSize().width, tlb_format.getPreferredSize().height);
-		tlb_quality.setBounds(273, 23, tlb_quality.getPreferredSize().width, tlb_quality.getPreferredSize().height);
-		tlb_path.setBounds(14, 80, tlb_path.getPreferredSize().width, tlb_path.getPreferredSize().height);
+		format.setBounds(26, 23, format.getPreferredSize().width, format.getPreferredSize().height);
+		quality.setBounds(273, 23, quality.getPreferredSize().width, quality.getPreferredSize().height);
+		path.setBounds(14, 80, path.getPreferredSize().width, path.getPreferredSize().height);
 
-		jft_path.setBounds(65, 76, 456, 22);
+		pathField.setBounds(65, 76, 456, 22);
 
 		cb_format.setBounds(83, 19, 96, 22);
 		cb_quality.setBounds(365, 19, 150, 22);
 
-		mainFrame.add(btn_browse);
+		mainFrame.add(browse);
 
-		mainFrame.add(tlb_format);
-		mainFrame.add(tlb_path);
-		mainFrame.add(tlb_quality);
+		mainFrame.add(format);
+		mainFrame.add(path);
+		mainFrame.add(quality);
 
-		mainFrame.add(jft_path);
+		mainFrame.add(pathField);
 
 		mainFrame.add(cb_format);
 		mainFrame.add(cb_quality);
