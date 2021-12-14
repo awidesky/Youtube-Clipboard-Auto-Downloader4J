8482a440fc904dbdf52f11d80d06a3b2786b48cb
Eugene Hong <awidesky@naver.com>
2021-12-14 14:24:31 +0900

    update jremaker to find proper jlink location

--- binarySetup.bat
+++ binarySetup.bat
@@ -4,14 +4,14 @@ powershell -Noprofile -Executionpolicy Unrestricted .\binarySetup.ps1
 
 echo.
 
-set /p YN="Want to make jre 11 for build Windows standalone application (Y/N)? "
+set /p YN="Want to make jre for build Windows standalone application (Y/N)? "
 
 if /i "%YN%" == "y" goto jre
 if /i "%YN%" == "n" goto end
 
 :jre
 echo.
-jremaker-11.0.12.bat
+jremaker.bat
 goto end
 
 :end
---
+++ jremaker.bat
@@ -0,0 +1,11 @@
+@echo on
+
+rmdir /s /q .\jre
+
+where jlink > jreLoc.txt
+set /p location=<jreLoc.txt
+del jreLoc.txt
+
+"%location%" --output jre --compress=2 --no-header-files --no-man-pages --module-path ../jmods --add-modules java.base,java.desktop
+
+@pause
