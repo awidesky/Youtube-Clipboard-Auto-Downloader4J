133c32ab1fa34369268e2c6f6520f0e4d27b1fdc
Eugene Hong <awidesky@naver.com>
2023-02-24 10:39:36 +0900

    java.datatransfer is in java.desktop

--- jremaker.bat
+++ jremaker.bat
@@ -6,6 +6,6 @@ where jlink > jreLoc.txt
 set /p location=<jreLoc.txt
 del jreLoc.txt
 
-"%location%" --output jre --compress=2 --no-header-files --no-man-pages --module-path ../jmods --add-modules java.base,java.desktop,java.datatransfer
+"%location%" --output jre --compress=2 --no-header-files --no-man-pages --module-path ../jmods --add-modules java.base,java.desktop
 
 @pause
