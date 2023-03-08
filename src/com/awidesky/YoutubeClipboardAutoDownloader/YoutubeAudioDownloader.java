934bf2e447d8a3c098c7e899d4c0c691372d99da
Eugene Hong <awidesky@naver.com>
2023-03-08 23:47:43 +0900

    add todo

--- src/com/awidesky/YoutubeClipboardAutoDownloader/YoutubeAudioDownloader.java
+++ src/com/awidesky/YoutubeClipboardAutoDownloader/YoutubeAudioDownloader.java
@@ -39,6 +39,8 @@ public class YoutubeAudioDownloader {
 		/* check ffmpeg */
 		Logger log = Main.getLogger("[ffmepg check] ");
 
+		//TODO : system PATH check
+		
 		// retrieve command line argument
 		log.log("Checking ffmpeg installation by \"" + youtubedlpath + "ffmpeg -version" + "\"");
 
