aea0e08b09eddb3cd196c0c42bad5c6d7a1edc03
Eugene Hong <awidesky@naver.com>
2024-09-21 17:43:18 +0900

    use lowercase in os detection

--- src/main/java/io/github/awidesky/YoutubeClipboardAutoDownloader/util/OSUtil.java
+++ src/main/java/io/github/awidesky/YoutubeClipboardAutoDownloader/util/OSUtil.java
@@ -2,10 +2,10 @@ package io.github.awidesky.YoutubeClipboardAutoDownloader.util;
 
 public class OSUtil {
 
-	private static final String OS = System.getProperty("os.name");
+	private static final String OS = System.getProperty("os.name").toLowerCase();
 
-    public static boolean isWindows() { return OS.contains("Windows"); }
-    public static boolean isMac() { return OS.contains("Mac"); }
-    public static boolean isLinux() { return OS.contains("Linux"); }
+    public static boolean isWindows() { return OS.contains("windows"); }
+    public static boolean isMac() { return OS.contains("mac"); }
+    public static boolean isLinux() { return OS.contains("linux"); }
     
 }
