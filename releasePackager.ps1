d7536da8509b832ea2c909e1789d6d455a006ee3
Eugene Hong <awidesky@naver.com>
2025-02-21 17:39:44 +0900

    add releasePackager.ps1

---
+++ releasePackager.ps1
@@ -0,0 +1,11 @@
+﻿$builds = ".\target\builds"
+
+Get-ChildItem -Path "$builds\jpackage" | Rename-Item -NewName {($_.Basename -replace "[\-_]\d\.\d\.\d","") + $_.extension}
+
+Compress-Archive -DestinationPath "$builds\Clipboard-dl.zip" -Path "$builds\Clipboard-dl\*"
+
+Compress-Archive -DestinationPath "$builds\Clipboard-dl_launch4j_jre.zip" -Path "$builds\launch4j\*"
+
+Remove-Item -Path $builds\launch4j\jre -Force -Recurse
+
+Compress-Archive -DestinationPath "$builds\Clipboard-dl_launch4j.zip" -Path "$builds\launch4j\*"
